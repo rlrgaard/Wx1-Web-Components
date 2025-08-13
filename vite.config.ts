d32449263ca769f98ac1c4da85457c6cc385a5a0
kevsimps <kevsimps@cisco.com>
2025-08-13 14:56:37 -0400

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
+export default defineConfig({
+    build:{
+        rollupOptions:{
+            output:{
+                entryFileNames:"[name].js",
+                chunkFileNames:"[name].js",
+                assetFileNames:"[name].[ext]"
+            }
+        }
+    },
+    plugins: [
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ]
+})
